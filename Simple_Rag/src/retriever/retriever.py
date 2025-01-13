ae6de6af7158fa153536e8b44fe01446a77a2e88
ehsan.tavan <tavan.ehsan@gamil.com>
2025-01-13 14:22:15 +0330

    Feature/Refactor create_retriever_chain function.

--- Simple_Rag/src/retriever/retriever.py
+++ Simple_Rag/src/retriever/retriever.py
@@ -1,20 +1,15 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-from .helper import get_milvus_collection
-
-
-def get_embedding_function(retriever_model_config):
-    return HuggingFaceEmbeddings(
-        model_name=retriever_model_config["model_name"],
-        model_kwargs=retriever_model_config["model_kwargs"],
-    )
+from Simple_Rag.src.embeddings import HuggingFaceEmbeddingModel
+from Simple_Rag.src.vector_store import MilvusVectorStore
 
 
 def create_retriever_chain(retriever_model_config):
-    retriever_model = get_embedding_function(retriever_model_config)
+    retriever_model = HuggingFaceEmbeddingModel(
+        model_name=retriever_model_config["model_name"],
+        model_kwargs=retriever_model_config["model_kwargs"]
+    ).model
 
     def _get_retriever_chain(retriever_config):
-
-        vector_store = get_milvus_collection(
+        vector_store = MilvusVectorStore(
             embedding_function=retriever_model,
             connection_args=retriever_config["retriever_db_config"],
             collection_name=retriever_config["collection_name"],
