e2b0ba2d86d9259e17129b05e578bb2832aff940
ehsan.tavan <tavan.ehsan@gamil.com>
2025-01-26 09:54:05 +0330

    Docs: Add docstring to AbstractVectorStore class.

--- Simple_Rag/src/vector_store/base.py
+++ Simple_Rag/src/vector_store/base.py
@@ -3,10 +3,25 @@ from langchain_core.vectorstores import VectorStore
 
 
 class AbstractVectorStore(ABC):
+    """
+    Abstract base class for a vector store.
+
+    This class provides an interface for converting a vector store
+    into a retriever, which can be used for search or retrieval tasks.
+    """
     @abstractmethod
     def as_retriever(
             self,
             search_type: str,
             search_kwargs: dict
     ) -> VectorStore:
-        """Convert vector store to a retriever."""
+        """
+        Convert the vector store into a retriever.
+
+        Args:
+            search_type: The type of search to perform (e.g., "similarity", ).
+            search_kwargs: Additional arguments for the search.
+
+        Returns:
+            A retriever instance derived from the vector store.
+        """
