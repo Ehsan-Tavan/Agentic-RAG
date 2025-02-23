e64a8e6e847a437f38757cbe2c9e35c147cfc011
ehsan.tavan <tavan.ehsan@gamil.com>
2025-02-23 15:38:32 +0330

    Add/add-get-section-formatter-node

--- Research_AI_Agent/src/graph/nodes/section_formatter.py
+++ Research_AI_Agent/src/graph/nodes/section_formatter.py
@@ -1,5 +1,26 @@
+from typing import Dict
+
+from Research_AI_Agent.src.graph.structures import Queries
 from Research_AI_Agent.src.graph.structures import Section
 from Research_AI_Agent.src.graph.state import ReportState
+
+
+class SectionFormatterNode:
+    def __init__(self, format_sections_fnc) -> None:
+        self.format_sections_fnc = format_sections_fnc
+
+    def __call__(self, state: ReportState) -> Dict[str, str]:
+        print('--- Formatting Completed Sections ---')
+
+        # List of completed sections
+        completed_sections = state["completed_sections"]
+
+        # Format completed section to str to use as context for final sections
+        completed_report_sections = self.format_sections_fnc(completed_sections)
+
+        print('--- Formatting Completed Sections is Done ---')
+
+        return {"report_sections_from_research": completed_report_sections}
 
 
 def format_sections(sections: list[Section]) -> str:
@@ -13,23 +34,13 @@ def format_sections(sections: list[Section]) -> str:
                             {section.description}
                             Requires Research:
                             {section.research}
-                            
+
                             Content:
                             {section.content if section.content else '[Not yet written]'}
-                            
+
                             """
     return formatted_str
 
 
-def build_completed_sections_string(state: ReportState):
-    print('--- Formatting Completed Sections ---')
-
-    # List of completed sections
-    completed_sections = state["completed_sections"]
-
-    # Format completed section to str to use as context for final sections
-    completed_report_sections = format_sections(completed_sections)
-
-    print('--- Formatting Completed Sections is Done ---')
-
-    return {"report_sections_from_research": completed_report_sections}
+def get_section_formatter_node() -> SectionFormatterNode:
+    return SectionFormatterNode(format_sections)
