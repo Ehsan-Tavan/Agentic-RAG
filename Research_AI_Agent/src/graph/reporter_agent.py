0a010215bb30e4c6ada63c387f2fd096038d1ca5
ehsan.tavan <tavan.ehsan@gamil.com>
2025-02-23 15:52:00 +0330

    Add/add-reporter-agent.

---
+++ Research_AI_Agent/src/graph/reporter_agent.py
@@ -0,0 +1,64 @@
+from typing import Dict, Union
+from langgraph.graph import StateGraph, START, END
+from langgraph.constants import Send
+
+from .state import ReportState, ReportStateInput, ReportStateOutput
+from .nodes.report_plan_section_generator import get_generate_report_plan_sections_node
+from .section_builder_agent import create_section_builder_sub_agent
+from .nodes.section_formatter import get_section_formatter_node
+from .nodes.finalize_sections_content import get_final_section_writer_node
+from .nodes.final_report_compiler import get_final_report_compiler_node
+
+
+def parallelize_section_writing(state: ReportState):
+    # Kick off section writing in parallel via Send() API for any sections that require research
+    return [
+        Send("section_builder_with_web_search",  # name of the subagent node
+             {"section": s})
+        for s in state["sections"]
+        if s.research
+    ]
+
+
+def parallelize_final_section_writing(state: ReportState):
+    # Kick off section writing in parallel via Send() API for any sections that do not require research
+    return [
+        Send("write_final_sections",
+             {"section": s, "report_sections_from_research": state["report_sections_from_research"]})
+        for s in state["sections"]
+        if not s.research
+    ]
+
+
+def create_reporter_agent(config: Dict[str, Union[str, int, float, Dict[str, str]]]):
+    generate_report_plan_node = get_generate_report_plan_sections_node(model_config=config["model_config"])
+    section_builder_agent = create_section_builder_sub_agent(config=config)
+    section_formatter_node = get_section_formatter_node()
+    final_section_writer_node = get_final_section_writer_node(model_config=config["model_config"])
+
+    builder = StateGraph(state_schema=ReportState, input=ReportStateInput, output=ReportStateOutput)
+    builder.add_node("generate_report_plan", generate_report_plan_node)
+    builder.add_node("section_builder_with_web_search", section_builder_agent)
+    builder.add_node("format_completed_sections", section_formatter_node)
+    builder.add_node("write_final_sections", final_section_writer_node)
+    builder.add_node("compile_final_report", get_final_report_compiler_node)
+
+    builder.add_edge(START, "generate_report_plan")
+
+    builder.add_conditional_edges("generate_report_plan",
+                                  parallelize_section_writing,
+                                  ["section_builder_with_web_search"])
+    builder.add_edge("section_builder_with_web_search", "format_completed_sections")
+    builder.add_conditional_edges("format_completed_sections",
+                                  parallelize_final_section_writing,
+                                  ["write_final_sections"])
+    builder.add_edge("write_final_sections", "compile_final_report")
+    builder.add_edge("compile_final_report", END)
+
+    reporter_agent = builder.compile()
+
+    plot = reporter_agent.get_graph(xray=True).draw_mermaid_png()
+    with open("final_plot.png", "wb") as fp:
+        fp.write(plot)
+
+    return reporter_agent
