d0fa774b1dd7ca12251d03f92874209814eb0191
ehsan.tavan <tavan.ehsan@gamil.com>
2025-03-09 16:04:24 +0330

    Refactor: Rename `final_plot.png` to `research_ai_agent.png`.

--- Research_AI_Agent/src/graph/research_ai_agent.py
+++ Research_AI_Agent/src/graph/research_ai_agent.py
@@ -75,7 +75,7 @@ def create_reporter_agent(config: Dict[str, Union[str, int, float, Dict[str, str
     reporter_agent = builder.compile()
 
     plot = reporter_agent.get_graph(xray=True).draw_mermaid_png()
-    with open("final_plot.png", "wb") as fp:
+    with open("research_ai_agent.png", "wb") as fp:
         fp.write(plot)
 
     return reporter_agent
